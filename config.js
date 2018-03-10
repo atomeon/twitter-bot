e2605fd6814e14e07da3ad041be6b0b021b33fb2
atomeon <atomeon@gmail.com>
2018-03-10 19:12:21 +0600

    remove confug.js from tracking

--- config.js
+++
@@ -1,7 +0,0 @@
- module.exports = {
-	"consumer_key": "omGjpcYKDp7rnqLdExnQiolMC",
-	"consumer_secret": "lpZx5aTmFPmvQgX0zO7YL7Ab9DXKGEEuq5Ev4djCO7mBXF6T2f",
-	"access_token": "2547611498-heSnD1g5ONBOaB2cwN9iBOedRRFh7YNLzP36UDO",
-	"access_token_secret": "QYegrHx4WQxZx2IsvshwTq7qQEWlhcxlS6EXSucJlk3a1",
-	// "callBackUrl": "XXX"
-}
